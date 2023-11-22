6c159eda9c9c029498673636a3f0d70427e4d0e2
DimaMen265 <menzak87@gmail.com>
2023-11-22 19:33:31 +0100

    Added Transaction History

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,8 @@ import { Statistics } from "./Statistics/Statistics";
 import data from "../to/data.json"
 import { FriendList } from "./FriendList/FriendList";
 import friends from "../to/friends.json";
+import { TransactionHistory } from "./TransactionHistory/TransactionHistory";
+import transactions from "../to/transactions.json";
 
 export const App = () => {
   return (
@@ -17,6 +19,7 @@ export const App = () => {
       />
       <Statistics title="Upload stats" data={data} />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
 }
